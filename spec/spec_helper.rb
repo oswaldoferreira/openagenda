f2ef80039c258f723f4364b8e426b0b2d2b449bd
Oswaldo Ferreira <oswluizf@gmail.com>
2015-12-19 19:15:46 -0200

    Setting up spec helper

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -1,9 +1,18 @@
 RSpec.configure do |config|
+  config.disable_monkey_patching!
+  config.order = :random
+  Kernel.srand config.seed
+  config.filter_run focus: true
+  config.run_all_when_everything_filtered = true
   config.expect_with :rspec do |expectations|
-    expectations.include_chain_clauses_in_custom_matcher_descriptions = true
+    expectations.syntax = :expect
   end
-
   config.mock_with :rspec do |mocks|
+    mocks.syntax = :expect
     mocks.verify_partial_doubles = true
   end
+  config.default_formatter = 'doc' if config.files_to_run.one?
+
+  config.example_status_persistence_file_path = "spec/examples.txt"
 end
+
