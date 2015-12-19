2a7f0a08f9941ba4a83f2368822c126fb2dd4220
Oswaldo Ferreira <oswluizf@gmail.com>
2015-12-19 19:16:05 -0200

    Removing user presence validation for now

--- app/models/event.rb
+++ app/models/event.rb
@@ -1,5 +1,5 @@
 class Event < ActiveRecord::Base
   belongs_to :user
 
-  validates :user, :title, presence: true
+  validates :title, presence: true
 end
--- spec/models/event_spec.rb
+++ spec/models/event_spec.rb
@@ -3,6 +3,5 @@ require 'rails_helper'
 RSpec.describe Event, type: :model do
   describe 'validations' do
     it { is_expected.to validate_presence_of(:title) }
-    it { is_expected.to validate_presence_of(:user) }
   end
 end
