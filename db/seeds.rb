6e5e7ae220aabd4b9dda9c21d292bb9c4457d569
Oswaldo Ferreira <oswluizf@gmail.com>
2015-12-25 21:32:08 -0200

    Initial seed

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,7 +1,8 @@
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the rake db:seed (or created alongside the db with db:setup).
-#
-# Examples:
-#
-#   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
-#   Mayor.create(name: 'Emanuel', city: cities.first)
+puts 'Creating events'
+
+unless Event.any?
+  Event.create!(title: 'Rubyconf talk', description: 'Talk about immutability on Ruby', start_at: DateTime.new(2016, 3, 20, 8, 0, 0))
+  Event.create!(title: 'JS conf talk', description: 'Talk about JS 2015', start_at: DateTime.new(2016, 3, 21, 8, 0, 0))
+  Event.create!(title: 'Hackaton', description: 'Some random hackaton', start_at: DateTime.new(2016, 3, 30, 8, 0, 0))
+  Event.create!(title: 'Meetup', description: 'Some random meetup', start_at: DateTime.new(2016, 4, 20, 8, 0, 0))
+end
